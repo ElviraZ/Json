76399fcca5aa5569dbfcd72cb54223b171c310e1
Elvira_Z <502768095@qq.com>
2017-04-25 13:23:31 +0800

    11

---
+++ Assets/Scripts/JsonUtilityDemo.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+[Serializable]
+public  class Person
+{
+    public string name;
+    public int age;
+}
+[Serializable]
+public class Persons
+{
+    public Person[] persons;
+  
+}
+public class JsonUtilityDemo : MonoBehaviour 
+   {
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //Json  两种操作方式：LitJson，  JsonUtility
+
+        //｛“name”：“李逍遥”，“age”：25｝
+        Person p1 = new Person();
+        p1.name = "李逍遥";
+        p1.age = 25;
+        //转成json字符串
+        string jsonStr = JsonUtility.ToJson(p1);
+        Debug.Log(jsonStr);
+
+
+        //{｛“name”：“李逍遥”，“age”：25｝，｛“name”：“王小虎”，“age”：7｝}
+        Person p2 = new Person();
+        p2.name = "王小虎";
+        p2.age = 7;
+        Person[] ps = new Person[]{p1, p2};
+        Persons persons = new Persons();
+        persons.persons = ps;
+        jsonStr = JsonUtility.ToJson(persons);
+        Debug.Log(jsonStr);
+    }
+
+}
