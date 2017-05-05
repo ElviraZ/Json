8e132eafe29ee1c3f557ddf91393571a3bdc8e7f
Elvira_Z <502768095@qq.com>
2017-05-05 17:40:17 +0800

    添加了LitJson

--- Assets/Scripts/JsonUtilityDemo.cs
+++ Assets/Scripts/JsonUtilityDemo.cs
@@ -24,13 +24,15 @@ public class JsonUtilityDemo : MonoBehaviour
 
         //Json  两种操作方式：LitJson，  JsonUtility
 
+        //一：使用JsonUtility
+        //创建
         //｛“name”：“李逍遥”，“age”：25｝
         Person p1 = new Person();
         p1.name = "李逍遥";
         p1.age = 25;
         //转成json字符串
         string jsonStr = JsonUtility.ToJson(p1);
-        Debug.Log(jsonStr);
+     //   Debug.Log(jsonStr);
 
 
         //{｛“name”：“李逍遥”，“age”：25｝，｛“name”：“王小虎”，“age”：7｝}
@@ -41,7 +43,16 @@ public class JsonUtilityDemo : MonoBehaviour
         Persons persons = new Persons();
         persons.persons = ps;
         jsonStr = JsonUtility.ToJson(persons);
-        Debug.Log(jsonStr);
+        //    Debug.Log(jsonStr);
+
+        //解析
+        Persons newPersons = JsonUtility.FromJson<Persons>(jsonStr);
+        for (int i = 0; i < newPersons.persons.Length; i++)
+        {
+            Debug.Log(newPersons.persons[i].name);
+            Debug.Log(newPersons.persons[i].age);
+        }
+      //  Debug.Log(newPersons.persons[0]);
     }
 
 }
---
+++ Assets/Scripts/LitJsonDemo.cs
@@ -0,0 +1,92 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+public  class  Hero
+{
+    public string name;
+    public int power;
+}
+public  class Heros
+{
+    public Hero[] heros;
+}
+   public class LitJsonDemo : MonoBehaviour 
+   {
+    // Use this for initialization
+    void Start () 
+	{
+        //  Func1();
+        //   Func2();
+        Fun3();
+    }
+
+    //一：使用JsonUtility
+    void  Func1()
+    {
+        //创建
+        //{heros｛“name”：“超人”，“power”：25｝，｛“name”：“蝙蝠侠”，“power”：7｝}
+        Hero hero1 = new Hero();
+        hero1.name = "超人";
+        hero1.power = 25;
+
+        Hero hero2 = new Hero();
+        hero2.name = "蝙蝠侠";
+        hero2.power = 7;
+        Heros heros = new Heros();
+        heros.heros = new Hero[] { hero1, hero2 };
+        string str = JsonMapper.ToJson(heros);
+        Debug.Log(str);
+        //解析
+     Heros  newheros=     JsonMapper.ToObject<Heros>(str);
+        for (int i = 0; i < newheros.heros.Length; i++)
+        {
+            Debug.Log(newheros.heros[i].name);
+            Debug.Log(newheros.heros[i].power);
+        }
+    }
+    void  Func2()
+    {
+
+      
+        //创建
+       // ｛“name”：“超人”，“power”：25｝
+        JsonData data = new JsonData();//json可以是[],也可以是{}
+        data.SetJsonType(JsonType.Object);
+        data["name"] = "超人";
+        data["power"] = 90;
+        data.ToJson();
+       // Debug.Log(data.ToJson());
+
+
+
+        //{heros｛“name”：“超人”，“power”：25｝，｛“name”：“蝙蝠侠”，“power”：7｝}
+        JsonData newdata = new JsonData();//{}
+        JsonData newdata1 = new JsonData();//{}
+        JsonData newdata2 = new JsonData();//{}
+        newdata1["name"] = "超人";
+        newdata1["power"] = 90;
+        newdata2["name"] = "蝙蝠侠";
+        newdata2["power"] = 70;
+
+        JsonData heros = new JsonData();//
+        heros.SetJsonType(JsonType.Array);
+        heros.Add(newdata1);
+        heros.Add(newdata2);
+        newdata["heros"] = heros;
+        //Debug.Log(newdata.ToJson());
+    }
+    void Fun3 ()
+    {
+        string str = "{'heros':[{'name':'超人','power':90},{'name':'蝙蝠侠','power':70}]}";
+   JsonData  herosjd    = JsonMapper.ToObject(str);
+        JsonData heros = herosjd["heros"];//[]
+        foreach (JsonData item in heros)
+        {
+            Debug.Log(item["name"].ToString());
+            Debug.Log(item["power"].ToString());
+        }
+      
+    }
+}
